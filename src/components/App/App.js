3269ab171cb01ac265d884e97004a939510630f1
MaxDaniLOVE <mahseeen@gmail.com>
2020-02-05 12:40:25 +0300

    init

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Header from '../Header'
+import './App.css';
+
+export default class App extends Component{
+  render() {
+    return (
+      <Header />
+    );
+  }
+};
