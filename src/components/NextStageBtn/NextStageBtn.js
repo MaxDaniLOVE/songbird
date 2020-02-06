9527f68ad57fbcbed909c24e1700470ff531f6fc
MaxDaniLOVE <mahseeen@gmail.com>
2020-02-06 13:25:49 +0300

    refactor: rewrite nextStageBtn

--- src/components/NextStageBtn/NextStageBtn.js
+++ src/components/NextStageBtn/NextStageBtn.js
@@ -1,26 +1,16 @@
 import React from 'react';
 
 const NextStageBtn = ({onNextBtnClick, isGuessed}) => {
-  let btnClassname;
-  if (isGuessed) {
-    return (
-      <button
-        type="button"
-        className="btn btn-primary next-btn"
-        onClick={onNextBtnClick}
-        >Next
-        </button>
-    );
-  } else {
-    return (
-      <button
-        type="button"
-        className="btn btn-primary next-btn disabled"
-        disabled
-        >Next
-        </button>
-    );
-  }
+  return (
+    <button
+      type="button"
+      className="btn btn-primary next-btn"
+      onClick={onNextBtnClick}
+      disabled={!isGuessed}
+    >
+      Next
+    </button>
+  );
 }
 
 export default NextStageBtn;
