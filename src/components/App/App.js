ef8edac3dd99e62ef23e6355c12ce018cf5ed9c7
MaxDaniLOVE <mahseeen@gmail.com>
2020-02-06 10:45:26 +0300

    feat: add birds list and it's card

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,40 @@ import Header from '../Header'
 import TopPanel from '../TopPanel';
 import GuessBirdBlock from '../GuessBirdBlock';
 import MainBlock from '../MainBlock';
+import birdsData from '../../services/birdsData';
+
 import './App.css';
 
 export default class App extends Component{
   state = {
     score: 0,
+    counter: 0,
+    displayedBird: null,
+    birdsData: birdsData[0]
   }
+
+  onSelectBird = (id) => {
+    this.setState(({ birdsData }) =>{
+      const idx = birdsData.findIndex((el) => el.id === id)
+      const displayedBird = birdsData[idx]
+      return {
+        displayedBird
+      }
+    })
+  }
+
   render() {
-    const { score } = this.state
+    const { score, birdsData, displayedBird } = this.state
     return (
       <div className="container">
         <Header score={score}/>
         <TopPanel />
         <GuessBirdBlock />
-        <MainBlock />
+        <MainBlock
+          onSelectBird={this.onSelectBird}
+          birdsList={birdsData}
+          displayedBird={displayedBird}
+        />
       </div>
     );
   }
--- src/components/MainBlock/MainBlock.js
+++ src/components/MainBlock/MainBlock.js
@@ -1,34 +1,80 @@
-import React from 'react';
-import './MainBlock.css';
+import React, { Component } from 'react';
+import './MainBlock.scss';
 
-const MainBlock = () => {
-  return (
-    <div className="main-block">
-      <div className="main-block-container">
-        <ul className="list-group bird-list">
-          <li className="list-group-item bird-list-item right">test</li>
-          <li className="list-group-item bird-list-item wrong">test</li>
-          <li className="list-group-item bird-list-item">test</li>
-          <li className="list-group-item bird-list-item">test</li>
-          <li className="list-group-item bird-list-item">test</li>
-        </ul>
-        <div className="card mb-3 bird-card">
-          <div className="card-header">Название на латыни</div>
-          <div className="card-container">
-            <img
-              src="https://birds-quiz.netlify.com/static/media/bird.06a46938.jpg"
-              alt="bird_image"/>
-            <div className="card-body">
-              <h4 className="card-title">Птица</h4>
-              <p className="card-text">Описание</p>
-              <audio controls/>
-            </div>
+export default class MainBlock extends Component {
+  state = {
+    birdsList: [],
+    displayedBird: null,
+  }
+
+  componentDidMount() {
+    this.updateInfo();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.displayedBird !== this.props.displayedBird) {
+      this.updateInfo();
+      setTimeout(() => {
+        this.refs.audio.pause(); // stops audio and update source when recieve new props
+        this.refs.audio.load();
+      }, 0);
+    }
+  } 
+
+  updateInfo() {
+    const { birdsList, displayedBird} = this.props;
+    this.setState({
+      birdsList,
+      displayedBird,
+    });
+  }
+
+  render() {
+    const { birdsList, displayedBird} = this.state;
+    const { onSelectBird } = this.props;
+    const list = birdsList.map(({name, id}) => {
+      return <li
+        key={id}
+        className="list-group-item bird-list-item"
+        onClick={() => onSelectBird(id)}
+        >
+          {name}
+        </li>
+    })
+    let birdBlock;
+    if (!displayedBird) {
+      birdBlock = <h3 className="select-bird">Выберите птицу из списка слева</h3>
+    } else {
+      const { audio, description, image, name, species } = displayedBird
+      birdBlock = <React.Fragment>
+                    <div className="card-header">{species}</div>
+                    <div className="card-container">
+                      <img
+                        className="selected-bird_image"
+                        src={image}
+                        alt="bird_image"/>
+                      <div className="card-body">
+                        <h4 className="card-title">{name}</h4>
+                        <p className="card-text">{description}</p>
+                        <audio controls ref="audio" className="birds-voice">
+                          <source src={audio}/>
+                        </audio>
+                      </div>
+                    </div>
+                  </React.Fragment>
+    }
+    return (
+      <div className="main-block">
+        <div className="main-block-container">
+          <ul className="list-group bird-list">
+            {list}
+          </ul>
+          <div className="card mb-3 bird-card">
+            {birdBlock}
           </div>
         </div>
+        <button type="button" className="btn btn-primary next-btn">Next</button>
       </div>
-      <button type="button" className="btn btn-primary next-btn">Next</button>
-    </div>
-  );
+    );
+  }
 }
-
-export default MainBlock;
